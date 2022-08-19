1df05edcbf390f0544eef6aa6e30e9216045c646
divyagovindaiah <110388603+divyagovindaiah@users.noreply.github.com>
2022-08-19 16:37:35 +0530

    Add files via upload

--- TelemetrymetricsTest.scala
+++ TelemetrymetricsTest.scala
@@ -2,13 +2,15 @@
 import org.scalatest.funsuite.AnyFunSuite
 class TelemetryMetricsTest extends AnyFunSuite {
   val telemetryObjects = TelemetryMetrics.telemetryObjects
+  val readfile = TelemetryMetrics.readFile()
+  val aggregate = TelemetryMetrics.aggregate()
   val completedContent = TelemetryMetrics.completedContent
 
+  println(telemetryObjects)
   test("TelemetryMetrics.countCompleted") {
     assert(TelemetryMetrics.countCompleted(telemetryObjects, 100.0, "80977d17-76ef-4507-b66c-11d4e5c2f045") == 1)
   }
 }
-
 
 
 
