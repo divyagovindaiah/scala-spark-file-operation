e2d167fcbc9a76e5002f2995bf675d090603e4d1
divyagovindaiah <110388603+divyagovindaiah@users.noreply.github.com>
2022-08-23 17:41:19 +0530

    Add files via upload

--- TelemetrymetricsTest.scala
+++ TelemetrymetricsTest.scala
@@ -6,8 +6,7 @@ class TelemetryMetricsTest extends AnyFlatSpec {
   val applicationConf = ConfigFactory.load("config1.conf")
   val sourceFile = applicationConf.getString("app.sourceFile")
   val readFile= TelemetryMetrics.readFile(sourceFile)
- val data = TelemetryMetrics.process(readFile)
-  println(readFile.readLine())
+  val data = TelemetryMetrics.process(readFile)
   it should "TelemetryMetrics.telemetryObjects" in {
     assert(1 === 1)
   }
